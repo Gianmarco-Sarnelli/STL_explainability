254a9319f5c56affe4c5c81f6e59064e26a56283
Gianmarco-Sarnelli <sarnelligianmarco@gmail.com>
2025-01-30 11:25:26 +0100

    Results from the completely new run

--- job_files/slurm_New_everything_1.sh
+++ job_files/slurm_New_everything_1.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=EPYC                     # Partition name
 #SBATCH --account=dssc                       # Account name
 #SBATCH --ntasks=1                           # Number of tasks (since we're using multiprocessing)
-#SBATCH --cpus-per-task=2                    # CPUs per task (for multiprocessing)
+#SBATCH --cpus-per-task=8                    # CPUs per task (for multiprocessing)
 #SBATCH --mem-per-cpu=2G                     # Memory per CPU
 #SBATCH --time=2:00:00                       # Time limit (2 hours)
 #SBATCH --output=output_1.log         # Standard output log
--- job_files/slurm_New_everything_8.sh
+++ job_files/slurm_New_everything_8.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=8                    # CPUs per task (for multiprocessing)
 #SBATCH --mem-per-cpu=2G                     # Memory per CPU
 #SBATCH --time=2:00:00                       # Time limit (2 hours)
-#SBATCH --output=output_0.log         # Standard output log
-#SBATCH --error=error_0.log           # Standard error log
+#SBATCH --output=output_8.log         # Standard output log
+#SBATCH --error=error_8.log           # Standard error log
 #SBATCH --get-user-env                       # Activating the environment
 
 # Activate the virtual environment
 source /u/dssc/gsarne00/Environments/expl_orfeo/bin/activate
 
 # Run the Python script
-python3 Test_distance_slurm.py job_files/params_New_everything_0.json New_everything 
+python3 Test_distance_slurm.py job_files/params_New_everything_8.json New_everything 

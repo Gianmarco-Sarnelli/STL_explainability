dd15065d378e8d6cd35f3d0e13ab1e678b1aa626
Gianmarco-Sarnelli <sarnelligianmarco@gmail.com>
2025-02-15 17:51:40 +0100

    solving time constraint

--- Running_multiple_tests.sh
+++ Running_multiple_tests.sh
@@ -14,7 +14,7 @@ for test_name in "${test_names[@]}"; do
 
             source /u/dssc/gsarne00/Environments/expl_orfeo/bin/activate
             #python3 Generate_jobs.py "$test_name" 20 "yes"
-            sbatch Slurm_Generate_jobs.sh "$test_name" 20 "yes"            
+            sbatch Slurm_Generate_jobs.sh "$test_name" 60 "yes"            
             # Additional wait to ensure file system sync
             sleep 30
             break
@@ -33,8 +33,9 @@ for test_name in "${test_names[@]}"; do
             echo "Starting job for test: $test_name"
             
             source /u/dssc/gsarne00/Environments/expl_orfeo/bin/activate
-            python3 Run_jobs.py --test_name "$test_name" --tests_num 20 --SLURM true
-            sleep 30
+            for i in {1..3}; do
+                python3 Run_jobs.py --test_name "$test_name" --tests_num 20 --SLURM true
+                sleep 30
 
             break
         else

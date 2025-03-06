911ec6e38119eb6f08cde11a1b709343b22562c3
Gianmarco-Sarnelli <sarnelligianmarco@gmail.com>
2025-03-06 19:57:17 +0100

    Results of the square root test

---
+++ job_files/slurm_square_root_MU0_M2B_37.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=EPYC                     # Partition name
+#SBATCH --account=dssc                       # Account name
+#SBATCH --ntasks=1                           # Number of tasks (since we're using multiprocessing)
+#SBATCH --cpus-per-task=8                    # CPUs per task (for multiprocessing)
+#SBATCH --mem-per-cpu=2G                     # Memory per CPU
+#SBATCH --time=2:00:00                       # Time limit (2 hours)
+#SBATCH --output=output_square_root_MU0_M2B_37.log         # Standard output log
+#SBATCH --error=error_square_root_MU0_M2B_37.log           # Standard error log
+#SBATCH --get-user-env                       # Activating the environment
+
+# Activate the virtual environment
+source /u/dssc/gsarne00/Environments/expl_orfeo/bin/activate
+
+# Run the Python script
+python3 Test_distance.py job_files/params_square_root_MU0_M2B_37.json square_root_MU0_M2B yes

af9c1610b9372fcbd0235d8b75f34ba8a22492e4
Gianmarco-Sarnelli <sarnelligianmarco@gmail.com>
2025-03-13 18:16:17 +0100

    runn

--- Slurm_Generate_jobs.sh
+++ Slurm_Generate_jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=THIN                     # Partition name
+#SBATCH --partition=EPYC                     # Partition name
 #SBATCH --account=dssc                       # Account name
 #SBATCH --ntasks=1                           # Number of tasks (for mpi, not needed now)
 #SBATCH --nodes=1                            # Use exactly one node
@@ -21,7 +21,7 @@ for test_name in "${test_names[@]}"; do
 
     echo "Generating jobs for test: $test_name"
 
-    python3 Generate_jobs.py "$test_name" 8 "yes" "THIN" "Test_model.py"
+    python3 Generate_jobs.py "$test_name" 8 "yes" "EPYC" "Test_model.py"
 
     echo "$test_name jobs are generated"
 

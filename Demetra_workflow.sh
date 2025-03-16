890fa0bf600208cbeb4d4f3e1bd3d5bfc8b4098a
Gianmarco-Sarnelli <sarnelligianmarco@gmail.com>
2025-03-16 13:20:39 +0100

    work now

--- Demetra_workflow.sh
+++ Demetra_workflow.sh
@@ -4,8 +4,17 @@
 generate_job_id=$(sbatch --parsable Demetra_Generate_jobs.sh)
 echo "Submitted Generate_jobs with job ID: $generate_job_id"
 
-# Wait for the job to complete
-srun --dependency=afterany:$generate_job_id --jobid=$generate_job_id true
+sleep 10
+
+
+# Check if job exists and wait for completion
+if squeue -j $generate_job_id &>/dev/null; then
+    scontrol wait jobid=$generate_job_id
+fi
+
+
+sleep 10
+
 
 echo "Job $generate_job_id completed, now processing RUNTHIS.txt"
 # Read the file line by line and add each line to the array file_list

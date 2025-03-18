7bfaa9edcd014578191e88209c95e169efb03eff
Gianmarco-Sarnelli <sarnelligianmarco@gmail.com>
2025-03-18 22:52:07 +0100

    results selfnorm_pure

---
+++ job_files/slurm_selfnorm_pure_M2M_0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --partition=THIN                     # Partition name
+#SBATCH --account=dssc                       # Account name
+#SBATCH --ntasks=1                           # Number of tasks (since we're using multiprocessing)
+#SBATCH --cpus-per-task=8                    # CPUs per task (for multiprocessing)
+#SBATCH --mem-per-cpu=2G                     # Memory per CPU
+#SBATCH --time=2:00:00                       # Time limit (2 hours)
+#SBATCH --output=output_selfnorm_pure_M2M_0.log         # Standard output log
+#SBATCH --error=error_selfnorm_pure_M2M_0.log           # Standard error log
+#SBATCH --get-user-env                       # Activating the environment
+
+# Activate the virtual environment
+source /u/dssc/gsarne00/Environments/expl_orfeo/bin/activate
+
+# Run the Python script
+python3 Test_model.py job_files/params_selfnorm_pure_M2M_0.json selfnorm_pure_M2M yes
